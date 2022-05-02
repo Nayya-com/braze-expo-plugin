c726f5c1eb5dafd7b4dd701dc510bc20e8d14c5b
farkmarnum <mark@nayya.com>
2022-05-02 03:10:34 -0400

    Add xcode library

--- src/global.d.ts
+++ src/global.d.ts
@@ -15,3 +15,12 @@ type OptionalKeys<T> = {
 }[keyof T];
 
 type RequiredProps = Exclude<OptionalKeys<ConfigProps>, undefined>;
+
+declare module 'xcode' {
+  interface xcode {
+    project(projPath: string): any;
+  }
+
+  const xcode: xcode;
+  export default xcode;
+}
