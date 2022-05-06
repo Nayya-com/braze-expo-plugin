70af9248c91f7d8b8d18e8cffb8edb84dd53dc3d
farkmarnum <mark@nayya.com>
2022-05-06 13:31:37 -0400

    Adjust MainApplication and MainActiviity changes to reflect latest Braze docs

--- src/android/withMainActivityModifications.ts
+++ src/android/withMainActivityModifications.ts
@@ -2,51 +2,6 @@ import { ConfigPlugin, withMainActivity } from '@expo/config-plugins';
 
 import { getLaunchMode } from './helpers/launchMode';
 
-const addImport = (stringContents: string): string => {
-  const importRegex = /^import [a-zA-Z.]+;/m;
-  const addedImport =
-    'import com.braze.BrazeActivityLifecycleCallbackListener;';
-
-  const match = stringContents.match(importRegex);
-  if (!match || match.index === undefined) {
-    throw new Error('Unable to match "import" in MainActivity');
-  }
-  const indexOfFirstImport = match.index;
-
-  stringContents = [
-    stringContents.slice(0, indexOfFirstImport),
-    addedImport,
-    stringContents.slice(indexOfFirstImport),
-  ].join('\n');
-
-  return stringContents;
-};
-
-const addToOnCreate = (stringContents: string): string => {
-  const onCreateRegex =
-    /(void onCreate\(.*\)(.|\n)*super.onCreate\(.*\);\s*\n)/;
-
-  const match = stringContents.match(onCreateRegex);
-  if (!match || match.index === undefined) {
-    throw new Error('Unable to match "void onCreate" in MainActivity');
-  }
-
-  const fullMatch = match[1];
-  const indexOfMatch = match.index;
-  const indexAfterMatch = indexOfMatch + fullMatch.length;
-
-  const addedLine =
-    '    registerActivityLifecycleCallbacks(new BrazeActivityLifecycleCallbackListener());';
-
-  stringContents = [
-    stringContents.slice(0, indexAfterMatch),
-    addedLine,
-    stringContents.slice(indexAfterMatch),
-  ].join('\n');
-
-  return stringContents;
-};
-
 const addToOnNewIntent = (stringContents: string): string => {
   const onNewIntentRegex =
     /(void onNewIntent\(Intent\s+([a-zA-Z]+)\s*\)(.|\n)*super.onNewIntent\(.*\);\s*\n)/;
@@ -78,8 +33,6 @@ export const withMainActivityModifications: ConfigPlugin = (configOuter) => {
     if (launchMode === 'singleTask') {
       let stringContents = config.modResults.contents;
 
-      stringContents = addImport(stringContents);
-      stringContents = addToOnCreate(stringContents);
       stringContents = addToOnNewIntent(stringContents);
 
       config.modResults.contents = stringContents;
--- src/android/withMainApplicationModifications.ts
+++ src/android/withMainApplicationModifications.ts
@@ -1,29 +1,56 @@
 import { ConfigPlugin, withMainApplication } from '@expo/config-plugins';
 
-export const withMainApplicationModifications: ConfigPlugin = (configOuter) => {
+const addImport = (stringContents: string): string => {
   const importRegex = /^import [a-zA-Z.]+;/m;
-  const addedImport = 'import com.appboy.AppboyLifecycleCallbackListener;';
+  const addedImport =
+    'import com.braze.BrazeActivityLifecycleCallbackListener;';
 
-  const onCreateRegex = /(void onCreate(.|\n)*super.onCreate\(.*\);)/;
-  const addedCode =
-    '    registerActivityLifecycleCallbacks(new AppboyLifecycleCallbackListener());';
+  const match = stringContents.match(importRegex);
+  if (!match || match.index === undefined) {
+    throw new Error('Unable to match "import" in MainActivity');
+  }
+  const indexOfFirstImport = match.index;
 
+  stringContents = [
+    stringContents.slice(0, indexOfFirstImport),
+    addedImport,
+    stringContents.slice(indexOfFirstImport),
+  ].join('\n');
+
+  return stringContents;
+};
+
+const addToOnCreate = (stringContents: string): string => {
+  const onCreateRegex =
+    /(void onCreate\(.*\)(.|\n)*super.onCreate\(.*\);\s*\n)/;
+
+  const match = stringContents.match(onCreateRegex);
+  if (!match || match.index === undefined) {
+    throw new Error('Unable to match "void onCreate" in MainActivity');
+  }
+
+  const fullMatch = match[1];
+  const indexOfMatch = match.index;
+  const indexAfterMatch = indexOfMatch + fullMatch.length;
+
+  const addedLine =
+    '    registerActivityLifecycleCallbacks(new BrazeActivityLifecycleCallbackListener());';
+
+  stringContents = [
+    stringContents.slice(0, indexAfterMatch),
+    addedLine,
+    stringContents.slice(indexAfterMatch),
+  ].join('\n');
+
+  return stringContents;
+};
+
+export const withMainApplicationModifications: ConfigPlugin = (configOuter) => {
   return withMainApplication(configOuter, (config) => {
     let stringContents: string = config.modResults.contents;
-    const match = stringContents.match(importRegex);
 
-    if (!match || match.index === undefined) {
-      throw new Error('Unable to match "import" in MainApplication');
-    }
-    const indexOfFirstImport = match.index;
-
-    stringContents = [
-      stringContents.slice(0, indexOfFirstImport),
-      addedImport,
-      stringContents.slice(indexOfFirstImport),
-    ].join('\n');
-
-    stringContents = stringContents.replace(onCreateRegex, `$1\n${addedCode}`);
+    stringContents = addImport(stringContents);
+    stringContents = addToOnCreate(stringContents);
 
     config.modResults.contents = stringContents;
 
