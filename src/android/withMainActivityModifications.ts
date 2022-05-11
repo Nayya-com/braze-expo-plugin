df05efedd1f2eabe839781838b34bc2b1cdf1459
farkmarnum <mark@nayya.com>
2022-05-11 12:35:46 -0400

    Fix onNewIntent

--- src/android/withMainActivityModifications.ts
+++ src/android/withMainActivityModifications.ts
@@ -2,17 +2,28 @@ import { ConfigPlugin, withMainActivity } from '@expo/config-plugins';
 
 import { getLaunchMode } from './helpers/launchMode';
 
-const addToOnNewIntent = (stringContents: string): string => {
-  const onNewIntentRegex =
-    /(void onNewIntent\(Intent\s+([a-zA-Z]+)\s*\)(.|\n)*super.onNewIntent\(.*\);\s*\n)/;
+const onNewIntentFullMethod = `@Override
+public void onNewIntent(Intent intent) {
+  super.onNewIntent(intent);
+  setIntent(intent);
+}`;
 
-  const match = stringContents.match(onNewIntentRegex);
-  if (!match || match.index === undefined) {
-    throw new Error('Unable to match "void onNewIntent" in MainActivity');
-  }
+const mainActivityClassRegex = /(public\s+class\s+MainActivity.*\{\s*?\n)/;
+
+const addOnNewIntent = (stringContents: string): string => {
+  return stringContents.replace(
+    mainActivityClassRegex,
+    `$1${onNewIntentFullMethod}`,
+  );
+};
+
+const addToExistingOnNewIntent = (
+  stringContents: string,
+  match: RegExpMatchArray,
+): string => {
   const fullMatch = match[1];
   const intentVariableName = match[2];
-  const indexOfMatch = match.index;
+  const indexOfMatch = match.index as number; // Type cast here since we've already checked this in addOnNewIntent below
   const indexAfterMatch = indexOfMatch + fullMatch.length;
 
   const addedLine = `    setIntent(${intentVariableName});`;
@@ -26,6 +37,20 @@ const addToOnNewIntent = (stringContents: string): string => {
   return stringContents;
 };
 
+const addOrModifyOnNewIntent = (stringContents: string): string => {
+  const onNewIntentRegex =
+    /(void onNewIntent\(Intent\s+([a-zA-Z]+)\s*\)(.|\n)*super.onNewIntent\(.*\);\s*\n)/;
+
+  const match = stringContents.match(onNewIntentRegex);
+  if (!match || match.index === undefined) {
+    // Method does not exist, so instead of adding to it, we'll need to add it in full:
+    return addOnNewIntent(stringContents);
+  }
+
+  // Method *does* exist, so we'll just add to it:
+  return addToExistingOnNewIntent(stringContents, match);
+};
+
 export const withMainActivityModifications: ConfigPlugin = (configOuter) => {
   return withMainActivity(configOuter, async (config) => {
     const launchMode = await getLaunchMode(config);
@@ -33,7 +58,7 @@ export const withMainActivityModifications: ConfigPlugin = (configOuter) => {
     if (launchMode === 'singleTask') {
       let stringContents = config.modResults.contents;
 
-      stringContents = addToOnNewIntent(stringContents);
+      stringContents = addOrModifyOnNewIntent(stringContents);
 
       config.modResults.contents = stringContents;
     }
